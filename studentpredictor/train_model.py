e17decf6fc3eb9efd3e0b70b2e0d9f1341eb21d7
Jieun Jeon <jieunjeon1023@gmail.com>
2022-02-01 13:32:35 -0800

    add Random Forest experiment

--- studentpredictor/train_model.py
+++ studentpredictor/train_model.py
@@ -1,16 +1,15 @@
 import pickle
 
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-
 from config import Config
+from sklearn.ensemble import RandomForestRegressor
+
+import pandas as pd
 
 Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
-
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
-
-model = LinearRegression()
+model = RandomForestRegressor(
+    n_estimators=150, max_depth=6, random_state=Config.RANDOM_SEED
+)
 model = model.fit(X_train, y_train.to_numpy().ravel())
-
 pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
